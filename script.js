5a8cf9c348a717fd8006647094a4e793e208de88
Juan Sanabria <89119697+Remixen@users.noreply.github.com>
2024-08-26 23:12:16 -0500

    Inicializamos el repositorio

---
+++ script.js
@@ -0,0 +1,67 @@
+/*  Declaramos variables*/
+
+const textArea = document.querySelector(".text-area") ;
+const mensaje = document.querySelector(".mensaje");
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada
+}
+
+function copiar() {
+    // Selecciona el área de texto donde está el mensaje encriptado
+    const textoEncriptado = document.getElementById("mensajeEncriptado");
+
+    // Selecciona el texto dentro del textarea
+    textoEncriptado.select();
+    textoEncriptado.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    // Usa la API del portapapeles para copiar el texto
+    navigator.clipboard.writeText(textoEncriptado.value)
+        .then(() => {
+            alert("Texto copiado al portapapeles");
+        })
+        .catch(err => {
+            console.error("Algo salió mal al copiar: ", err);
+        });
+}
